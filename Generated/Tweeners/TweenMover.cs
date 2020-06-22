8ff68f84ed35cf039901ede0c0920785e701380d
Vladislav Kinyashov <indraayy322@gmail.com>
2020-06-22 13:39:04 +0500

    Update TweenMover.cs

--- Generated/Tweeners/TweenMover.cs
+++ Generated/Tweeners/TweenMover.cs
@@ -7,9 +7,9 @@ namespace ToolBox.Reactors
 {
 	public sealed class TweenMover : Tweener
 	{
-		[SerializeField, Required, SceneObjectsOnly] private Transform _transform = null;
-		[SerializeReference, Required] private IPositionSelector _positionSelector = null;
-		[SerializeField] private float _duration = 1f;
+		[SerializeField, Required, SceneObjectsOnly, FoldoutGroup("Data")] private Transform _transform = null;
+		[SerializeReference, Required, FoldoutGroup("Data")] private IPositionSelector _positionSelector = null;
+		[SerializeField, FoldoutGroup("Data")] private float _duration = 1f;
 
 		protected override Tween RunTween() =>
 			_transform.DOMove(_positionSelector.GetPosition(), _duration);
